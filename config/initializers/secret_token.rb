6d181945a4887c7bf7b8cc800900955b113483f9
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-04-23 20:30:20 +0900

    秘密トークンを動的に生成
    ```
    $ vim config/initializers/secret_token.rb
    ```

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleAppParusu::Application.config.secret_key_base = 'c26d1904c29ff6ec359c60606c7165d23c20a6d0d175e07e23d46344d3cc630df363b97c62ae5b02167442cb4fe0f5dc7ae123f6d2eecfc967c130936bb3d8c0'
+SampleApp::Application.config.secret_key_base = secure_token
