f9fdd6ecb7e5ab202009dc4cd1719adb3ed42143
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-04-23 21:05:19 +0900

    テストのスペルミスを修正
    ```
    $ vim spec/requests/static_pages_spec.rb
    ```

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static Pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to hove_content('Sample App')
+      expect(page).to have_content('Sample App')
     end
   end
 end
