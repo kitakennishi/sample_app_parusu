66c3f110fd09087379b2f66a65a17417f79d3550
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-04-23 20:13:52 +0900

    rails new
    ```
    $ rails new . --skip-test-unit
    ```

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleAppParusu::Application.config.secret_key_base = 'c26d1904c29ff6ec359c60606c7165d23c20a6d0d175e07e23d46344d3cc630df363b97c62ae5b02167442cb4fe0f5dc7ae123f6d2eecfc967c130936bb3d8c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppParusu::Application.config.session_store :cookie_store, key: '_sample_app_parusu_session'
