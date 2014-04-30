b55888947fdb6cbc8048273cd36627d6f0767358
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-04-30 19:52:05 +0900

    サインインに成功した時のテストを追加
    ```
    $ vim spec/requests/authentication_pages_spec.rb
    ```

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,5 +20,19 @@ describe "Authentication" do
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
     end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+    end
+
+    it { should have_title(user.name) }
+    it { should have_link('Profile', href: user_path(user)) }
+    it { should have_link('Sign out', href: sign_path) }
+    it { should_not have_link('Sign in', href: sign_path) }
   end
 end
