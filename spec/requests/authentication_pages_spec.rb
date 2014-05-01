d6636fe0fad40bf4945291ea792adba38a3cf1b8
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-01 20:07:24 +0900

    サインイン成功時のテストを間違えていたので修正
    ```
    $ vim spec/requests/authentication_pages_spec.rb
    ```

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,11 +33,11 @@ describe "Authentication" do
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
-    end
 
-    it { should have_title(user.name) }
-    it { should have_link('Profile', href: user_path(user)) }
-    it { should have_link('Sign out', href: sign_path) }
-    it { should_not have_link('Sign in', href: sign_path) }
+      it { should have_title(user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
   end
 end
