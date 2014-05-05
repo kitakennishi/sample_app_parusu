d0ee15fb6c714f2ad87dfc82be0c7dc9c3bacf54
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-05 19:05:09 +0900

    userモデルにfeedメソッドを実装
    ```
    $ vim app/models/user.rb
    ```

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
+
+   def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
   def create_remember_token
     self.remember_token = User.encrypt(User.new_remember_token)
